72ad6d99e6d289484e63bdb761c59c27f45a0b43
Puneet Saini <puneet29saini@gmail.com>
2019-05-30 12:32:40 +0530

    Code fix index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const electron = require('electron')
+const electron = require('electron');
 
-const { app } = electron
+const { app } = electron;
 
 app.on('ready', () => {
     console.log("App is ready")
-})
+});
