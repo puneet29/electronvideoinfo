cfa8c05ecda9e50c6f3a217657fe52150ae9ea19
Puneet Saini <puneet29saini@gmail.com>
2019-05-30 12:38:21 +0530

    Empty Browser Window

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const electron = require('electron');
 
-const { app } = electron;
+const { app, BrowserWindow } = electron;
 
 app.on('ready', () => {
-    console.log("App is ready")
+    new BrowserWindow({});
 });
