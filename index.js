3f075c6c2a0ed8839b5f8bdc1db8f37317957bf1
Puneet Saini <puneet29saini@gmail.com>
2019-05-30 14:04:44 +0530

    Added backend

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const ffmpeg = require('fluent-ffmpeg');
 
 const { app, BrowserWindow, ipcMain } = electron;
 
+let mainWindow;
+
 app.on('ready', () => {
-    const mainWindow = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         webPreferences: {
             nodeIntegration: true
         }
@@ -12,8 +14,11 @@ app.on('ready', () => {
     mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
+// Get the duration of the video sent by renderer app
+
 ipcMain.on('video:submit', (event, path) => {
     ffmpeg.ffprobe(path, (err, metadata) => {
-        console.log(metadata.format.duration);
+        // Send the duration to renderer app
+        mainWindow.webContents.send('video:fetchDuration',metadata.format.duration);
     });
 });
