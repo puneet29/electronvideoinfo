a38a98d27f7dd32af0edaf8ac37fabae62bc963f
Puneet Saini <puneet29saini@gmail.com>
2019-05-30 14:22:38 +0530

    Documented the application

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Electron App Beginner
 
+An electron app that will help understand the basics of electron, its syntax, and workflow.
+This app illustrates the way electron app is built. How it uses IPC(Inter Process Communication) to fetch the length of video using fluent-ffmpeg which is a node js library.
+
 ## Getting Started
 
 1. Install npm (if not):
--- index.js
+++ index.js
@@ -1,24 +1,33 @@
+/*jshint esversion: 6 */
+
+// Importing the dependencies
 const electron = require('electron');
 const ffmpeg = require('fluent-ffmpeg');
 
+// Getting the properties from dependencies by destructuring
 const { app, BrowserWindow, ipcMain } = electron;
 
+// Defining main window
 let mainWindow;
 
+// Event handling: triggers when app is ready
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
         webPreferences: {
             nodeIntegration: true
         }
     });
-    mainWindow.loadURL(`file://${__dirname}/index.html`);
+    mainWindow.loadURL(`file://${__dirname}/index.html`); // load the HTML window
 });
 
+// Communication between index.html and index.js
 // [2nd step of IPC]Get the duration of the video sent by renderer app
 
 ipcMain.on('video:submit', (event, path) => {
     ffmpeg.ffprobe(path, (err, metadata) => {
+
         // [3rd step of IPC]Send the duration to renderer app
-        mainWindow.webContents.send('video:fetchDuration',metadata.format.duration);
+        mainWindow.webContents.send('video:fetchDuration', metadata.format.duration);
+
     });
 });
